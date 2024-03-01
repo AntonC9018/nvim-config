ea19defa043e96bcd4d7d57bc19f5e50ec0f9ddb
AntonC9018 <anton.curm@gmail.com>
2024-03-01 21:28:20 +0200

    Queries don't work?

---
+++ queries/latex/injections.scm
@@ -0,0 +1,11 @@
+(
+ (minted_environment)
+    begin: (begin)
+     name: (curly_group_text)
+      text: (text) 
+       word: (word)
+     language: (curly_group_text)
+      text: (text)
+       word: (word) @injection.language
+    code: (source_code) @injection.content
+)
